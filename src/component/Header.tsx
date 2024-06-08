03fb1832a43c9811ba5700f999a50fe999d718e9
1F10220146 <s1f102201469@iniad.org>
2024-06-09 00:31:12 +0900

    feat: add Header component

---
+++ src/component/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Header.module.css';
+import { LinkIcon } from './LinkIcon';
+import { FaCode, FaHome, FaUser } from 'react-icons/fa';
+import Link from './Link';
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <LinkIcon icon={<FaHome />} href="/" />
+      <div className={styles.iconLinkContainer} data-pagefind-ignore="index">
+        <Link href="/about" icon={<FaUser />} text="About" />
+        <Link href="/works" icon={<FaCode />} text="Works" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
