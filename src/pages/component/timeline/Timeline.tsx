03447735d72429f69f29f749e71c6f1ac05c3383
1F10220146 <s1f102201469@iniad.org>
2024-06-08 21:25:17 +0900

    feat: add Timeline component

---
+++ src/pages/component/timeline/Timeline.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import TimelineItem from './TimelineItem';
+
+const Timeline = () => {
+  return (
+    <div>
+      <TimelineItem
+        title="New branch"
+        description="You've created new branch fix-notifications from master"
+        time="2 hours ago"
+      />
+      <TimelineItem
+        title="Commits"
+        description="You've pushed 23 commits to fix-notifications branch"
+        time="52 minutes ago"
+      />
+      <TimelineItem
+        title="Pull request"
+        description="You've submitted a pull request Fix incorrect notification message (#187)"
+        time="34 minutes ago"
+      />
+      <TimelineItem
+        title="Code review"
+        description="Robert Gluesticker left a code review on your pull request"
+        time="12 minutes ago"
+        lastItem={true}
+      />
+    </div>
+  );
+};
+
+export default Timeline;
---
+++ src/pages/component/timeline/TimelineItem.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './TimelineItem.module.css';
+
+interface TimelineItemProps {
+  title: string;
+  description: string;
+  time: string;
+  lastItem?: boolean;
+}
+
+const TimelineItem: React.FC<TimelineItemProps> = ({
+  title,
+  description,
+  time,
+  lastItem = false,
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.timeline}>
+        <div className={styles.circle} style={{ marginBottom: lastItem ? '0' : 'auto' }} />
+      </div>
+      <div className={styles.details}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description}</div>
+        <div className={styles.time}>{time}</div>
+      </div>
+    </div>
+  );
+};
+
+export default TimelineItem;
