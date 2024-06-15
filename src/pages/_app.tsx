449255c6db784d2b60b9e18375586b12cd3e6fb4
1F10220146 <s1f102201469@iniad.org>
2024-06-16 05:41:43 +0900

    feat: Add MantineProvider to _app.tsx and ColorSchemeScript to _document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { MantineProvider } from '@mantine/core';
+import { DEFAULT_THEME } from '@mantine/core';
+import '@mantine/core/styles.css';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={DEFAULT_THEME}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
 export default MyApp;
