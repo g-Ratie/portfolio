fccff6ce06c46b9d02fbf92f6252dc723358c003
1F10220146 <s1f102201469@iniad.org>
2024-06-18 16:12:39 +0900

    fix: remove unused import

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { LinkIcon } from './LinkIcon';
-import { FaCode, FaFileCode, FaHome, FaUser } from 'react-icons/fa';
+import { FaFileCode, FaHome, FaUser } from 'react-icons/fa';
 import Link from './Link';
 
 const Header = () => {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,11 @@ import { MantineProvider } from '@mantine/core';
 import { DEFAULT_THEME } from '@mantine/core';
 import '@mantine/core/styles.css';
 
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={DEFAULT_THEME}>
       <Component {...pageProps} />
     </MantineProvider>
-  )
+  );
 }
 export default MyApp;
