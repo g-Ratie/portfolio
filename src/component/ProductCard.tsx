45a9335b1c1b0727194558a12b798e81a084596f
Yoshiki <s1f102201469@iniad.org>
2024-06-21 03:15:17 +0900

    feat: Update ProductCard component to accept dynamic data

--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -2,15 +2,6 @@ import { Card, Image, Text, Group, Badge, Button, ActionIcon } from '@mantine/co
 import { FaGithub } from 'react-icons/fa';
 import classes from './ProductCard.module.css';
 
-const mockdata = {
-  image:
-    'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80',
-  title: 'Verudela Beach',
-  country: 'Croatia',
-  description:
-    'Completely renovated for the season 2020, Arena Verudela Bech Apartments are fully equipped and modernly furnished 4-star.',
-};
-
 type Tag = {
   title: string;
   color: string;
@@ -24,9 +15,7 @@ export type ProductCardProps = {
   tag: Tag;
 };
 
-export const ProductCard = () => {
-  const { image, title, description, country } = mockdata;
-
+export const ProductCard = ({ image, title, description, tag }: ProductCardProps) => {
   return (
     <Card withBorder radius="md" p="md" className={classes.card}>
       <Card.Section>
@@ -38,8 +27,8 @@ export const ProductCard = () => {
           <Text fz="lg" fw={500}>
             {title}
           </Text>
-          <Badge size="sm" variant="light">
-            {country}
+          <Badge size="sm" variant="light" color={tag.color}>
+            {tag.title}
           </Badge>
         </Group>
         <Text fz="sm" mt="xs">
