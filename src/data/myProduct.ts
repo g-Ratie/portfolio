c4e27406154eb4d41b4a90d6274ed9cd1dfae317
Yoshiki <s1f102201469@iniad.org>
2024-06-21 03:43:40 +0900

    feat: Add Works page with ProductCard component and data

---
+++ src/data/myProduct.ts
@@ -0,0 +1,103 @@
+// eslint-disable-next-line no-sparse-arrays
+
+const cardsData = [
+  {
+    image: 'https://picsum.photos/500?random=33',
+    githubUrl: 'http://github.com',
+    title: 'VR授業スタジオ',
+    description:
+      'コロナ禍の中で「より良い教え合い」を求めて試作したVR授業プラットフォーム。これをテーマに全国高校生 MY PROJECT AWARD 2020 関東Summitに出場しました。',
+    tag: {
+      title: '個人開発',
+      color: '#FF5733',
+    },
+  },
+  {
+    image: 'https://picsum.photos/500?random=22',
+    githubUrl: null,
+    title: 'オセロ',
+    description: 'React学習のために制作したオセロです。オセロの基本的な機能を実装しました。',
+    tag: {
+      title: '個人開発',
+      color: '#33C1FF',
+    },
+  },
+  {
+    image: 'https://picsum.photos/500?random=44',
+    githubUrl: '',
+    title: 'マインスイーパー',
+    description:
+      'React学習のために制作したマインスイーパーです。マインスイーパーの基本的な機能を実装しました。',
+    tag: {
+      title: '個人開発',
+      color: '#33C1FF',
+    },
+  },
+  {
+    image: 'https://picsum.photos/500?random=11',
+    githubUrl: null,
+    title: 'INIAD.ts 入退室管理システム',
+    description:
+      'サークル入退室などを管理するためのDiscord Bot。サークルの活動場所であるオフィスの入室・退室を記録するために、サークルのDiscord上で動くBot(3代目)',
+    tag: {
+      title: '個人開発',
+      color: '#28A745',
+    },
+  },
+  {
+    image: 'https://picsum.photos/500?random=55',
+    githubUrl: 'https://github.com/g-Ratie/Network-reversi',
+    title: 'オンラインオセロ',
+    description:
+      'フルスタック開発の勉強として、前に開発したオセロをオンライン対戦できるように改良しました。',
+    tag: {
+      title: '個人開発',
+      color: '#FFC300',
+    },
+  },
+  {
+    image: 'https://picsum.photos/500?random=66',
+    githubUrl: '',
+    title: 'しらばすチャット(仮)',
+    description: '大学のシラバスを検索するRAGアプリケーション',
+    tag: {
+      title: '個人開発',
+      color: '#FF5733',
+    },
+  },
+  {
+    image: 'https://picsum.photos/500?random=77',
+    githubUrl: null,
+    title: 'リハビリ施設向けサイクリングデバイス',
+    description:
+      'エアロバイクと連動した、高齢者向けバーチャルサイクリングデバイスで、企業様と共同開発した作品です',
+    tag: {
+      title: '共同開発',
+      color: '#FF5733',
+    },
+  },
+  {
+    image: 'https://picsum.photos/500?random=77',
+    githubUrl: null,
+    title: 'AuralRx',
+    description:
+      '技育CAMPキャラバンハッカソン@東京で開発した、ヘルスケアデータの活動履歴からその日を振り返るプレイリストを作成するアプリケーション',
+    tag: {
+      title: 'チーム開発',
+      color: '#FF5733',
+    },
+  },
+  {
+    image: 'https://picsum.photos/500?random=77',
+    githubUrl: null,
+    title: 'One tap Schedule',
+    description:
+      '技育CAMP2024 マンスリーハッカソン Vol.4で開発した、Webサイト上の日時をワンクリックでカレンダーに追加できる拡張機能',
+    tag: {
+      title: 'チーム開発',
+      color: '#FF5733',
+    },
+  },
+];
+
+export default cardsData;
