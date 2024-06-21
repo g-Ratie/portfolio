75ea17a479d2b52ea8f9a79bf98e4618076c8c9d
Yoshiki <s1f102201469@iniad.org>
2024-06-22 04:32:39 +0900

    feat: Remove ActivityTimeline component from Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Layout from '../layouts/Layout';
-import ActivityTimeline from '../component/timeline/Timeline';
 import Title from '../component/Title';
-import myActivity from '../data/myactivity';
 
 const App: React.FC = () => {
   return (
     <Layout>
       <Title title="Home" />
-      <ActivityTimeline timeline={myActivity.myactivity} />
     </Layout>
   );
 };
