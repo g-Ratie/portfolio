41c05649dd725907e720147cc47774a48eb2d230
Yoshiki <s1f102201469@iniad.org>
2024-06-22 04:30:46 +0900

    feat: Add About page with content and activity timeline

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Title from '../../component/Title';
+import Layout from '../../layouts/Layout';
+import Content from '../../component/about/Section';
+import ActivityTimeline from '../../component/timeline/Timeline';
+import myActivity from '../../data/myactivity';
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Title title="About" />
+      <Content
+        sections={[
+          {
+            title: '自己紹介',
+            text: 'ラティといいます。Web開発が好きで、TypeScriptを使っていろいろするのが好きです。',
+          },
+          {
+            title: '興味',
+            text: 'TypeScript / React / Next.js / Bun / CloudFlare Workers / GitHub Actions',
+          },
+          {
+            title: '好き',
+            text: 'キーボード, ガジェット, アイドルマスター(シャニマス | 学マス), 少女☆歌劇 レヴュースタァライト',
+          },
+        ]}
+      />
+      <ActivityTimeline timeline={myActivity.myactivity} />
+    </Layout>
+  );
+};
+
+export default App;
