66a7626e334f088d2b81304461bf78c422bf0c13
Yoshiki <s1f102201469@iniad.org>
2024-07-16 13:56:12 +0900

    add: 最新の活動実績を追加

--- src/data/myactivity.ts
+++ src/data/myactivity.ts
@@ -6,6 +6,13 @@ const myActivity = {
       date: '2024/03 ~ 現在',
     },
     {
+      title: '技育CAMP2024 マンスリーハッカソン Vol.8 (サポーターズ主催)',
+      content:
+        '青空文庫の小説に挿絵・VOICEVOXによる読み上げを追加して楽しめるアプリケーションを開発しました',
+      date: '2024/04',
+      isprize: true,
+    },
+    {
       title: '技育CAMP2024 マンスリーハッカソン Vol.4 (サポーターズ主催)',
       content:
         'Webサイト上の日時をワンクリックでGoogleカレンダーに追加できるChrome拡張機能を開発しました。',
